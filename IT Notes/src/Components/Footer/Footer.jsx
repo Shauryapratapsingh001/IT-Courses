5ce88ac0f9dc32dc916672f3b0d9eb781ea3f720
Shaurya Singh <shauryapsingh000@gmail.com>
2026-05-06 08:46:08 +0530

    footer

--- IT Notes/src/Components/Footer/Footer.jsx
+++ IT Notes/src/Components/Footer/Footer.jsx
@@ -1,32 +1,69 @@
 import React from 'react'
-import '../Footer/Footer.css'
+import './Footer.css'
 
 export const Footer = () => {
   return (
-    <>
-       <section className='footer-container'>
-        <div>
-         <div className='footer-one'>
-            <h1>Lets Connected</h1>
-            <p>© 2024 Your Website Name. All Rights Reserved.</p>
+    <div className='footer-container'>
 
-         </div>
-         <div className='footer-two'>
-             <li> <nav></nav></li>
-    
-         </div>
-         <div className='footer-three'>
+      <footer className="footer">
+        <nav>
+          <h6 className="footer-title">Services</h6>
+          <a className="link link-hover">Branding</a>
+          <a className="link link-hover">Design</a>
+          <a className="link link-hover">Marketing</a>
+          <a className="link link-hover">Advertisement</a>
+        </nav>
+        <nav>
+          <h6 className="footer-title">Company</h6>
+          <a className="link link-hover">About us</a>
+          <a className="link link-hover">Contact</a>
+          <a className="link link-hover">Jobs</a>
+          <a className="link link-hover">Press kit</a>
+        </nav>
+        <nav>
+          <h6 className="footer-title">Legal</h6>
+          <a className="link link-hover">Terms of use</a>
+          <a className="link link-hover">Privacy policy</a>
+          <a className="link link-hover">Cookie policy</a>
+        </nav>
+        <nav>
+          <h6 className="footer-title">Social</h6>
+          <div className="social-grid">
+            <a>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                className="fill-current">
+                <path d="M24 4.557c-.883..."></path>
+              </svg>
+            </a>
+            <a>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                className="fill-current">
+                <path d="M19.615 3.184..."></path>
+              </svg>
+            </a>
+            <a>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                className="fill-current">
+                <path d="M9 8h-3v4h3v12..."></path>
+              </svg>
+            </a>
+          </div>
+        </nav>
+      </footer>
 
-         </div>
-         <hr></hr>
-         </div>
-         <div>
-           
-         </div>
-         
-       </section>
-    </>
+    </div>
   )
 }
-
 
